838a7c5663231d32bef4da0ec25cad34221496d7
BlushedNanis <luis_danieltg@outlook.com>
2024-04-14 16:05:34 -0400

    Added object detection

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-import cv2
+import cv2 as cv
 from time import sleep
 
-video = cv2.VideoCapture(0)
-sleep(1)
+video = cv.VideoCapture(0)
+sleep(0.5)
+
+first_frame = None
 
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray_gaus_frame = cv.GaussianBlur(gray_frame, (21,21), 0)
     
-    key = cv2.waitKey(1)
+    if first_frame is None:
+        first_frame = gray_gaus_frame
+    
+    delta_frame = cv.absdiff(first_frame, gray_gaus_frame)
+    thresh_frame = cv.threshold(delta_frame, 30, 255, cv.THRESH_BINARY)[1]
+    dil_frame = cv.dilate(thresh_frame, None, iterations=2)
+    
+    contours, check = cv.findContours(dil_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    
+    for contour in contours:
+        if cv.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv.boundingRect(contour)
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
+    cv.imshow("video", frame)
+    
+    key = cv.waitKey(1)
     
     if key == ord("q"):
         break
