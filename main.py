92ad5cc28b76e6516ca9918057c15d68ea6d29d5
BlushedNanis <luis_danieltg@outlook.com>
2024-04-14 20:35:20 -0400

    Catch image of the object to be sent through email

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ sleep(0.5)
 
 first_frame = None
 status_list = []
+max_rectangle = 0
 
 while True:
     status = 0
+    rectangle_area = 0
     
     check, frame = video.read()
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -25,18 +27,26 @@ while True:
     contours, check = cv.findContours(dil_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
-        if cv.contourArea(contour) < 5000:
+        if cv.contourArea(contour) < 2000:
             continue
         x, y, w, h = cv.boundingRect(contour)
         rectangle = cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
         if rectangle.any():
             status = 1
-        
+            rectangle_area = int(w * h / 100)
+    
     status_list.append(status)
     status_list = status_list[-2:]
         
+    if rectangle_area > max_rectangle:
+        cv.imwrite("image.png", frame)
+        max_rectangle = rectangle_area
+        print(max_rectangle)
+            
     if status_list[0] == 1 and status_list[1] == 0:
         send_email()
+        max_rectangle = 0
         
     cv.imshow("video", frame)
     
