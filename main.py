89a812b0f6ad5a70dd59e3a5db7c0842e5df9cb6
BlushedNanis <luis_danieltg@outlook.com>
2024-05-29 16:19:51 -0400

    Added comments

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ while True:
         # Set reference frame
         first_frame = gray_gaus_frame
     
+    # Make transformation to the current frame
     delta_frame = cv.absdiff(first_frame, gray_gaus_frame)
     thresh_frame = cv.threshold(delta_frame, 30, 255, cv.THRESH_BINARY)[1]
     dil_frame = cv.dilate(thresh_frame, None, iterations=2)
@@ -36,12 +37,14 @@ while True:
     contours, check = cv.findContours(dil_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
+        # Set bounding box
         if cv.contourArea(contour) < 2000:
             continue
         x, y, w, h = cv.boundingRect(contour)
         rectangle = cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         
         if rectangle.any():
+            # Set status to 1 if there is an object detected
             status = 1
             rectangle_area = int(w * h / 100)
     
@@ -49,10 +52,12 @@ while True:
     status_list = status_list[-2:]
         
     if rectangle_area > max_rectangle:
+        # Save the frame which have the biggest object area
         cv.imwrite("image.png", frame)
         max_rectangle = rectangle_area
             
     if status_list[0] == 1 and status_list[1] == 0:
+        # When there is an objet detected, send emil and reset object rectangle
         email_thread = Thread(target=send_email)
         email_thread.daemon = True
         email_thread.start()
